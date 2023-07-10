5de794ff49a5d3818872cf3b4eb4d733a03deb0b
petrutadarius <99864762+petrutadarius@users.noreply.github.com>
2023-07-10 23:02:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OneTimePasswordGenerator
+{
+    public class OneTimePasswordGenerator
+    {
+        private const int ValidityPeriodSeconds = 30;
+        public string GenerateOTP(string userId, DateTime dateTime)
+        {
+            string userIdString = userId.ToString();
+            string dateTimeString = dateTime.ToString("yyyyMMddHHmmss");
+
+            // Calculate the current time slot based on the validity period
+            long timeSlot = dateTime.Ticks / (ValidityPeriodSeconds * TimeSpan.TicksPerSecond);
+
+            // Concatenate user ID, date-time string, and time slot
+            string combinedString = userIdString + dateTimeString + timeSlot;
+
+            // Generate a hash code using the combined string
+            int hashCode = combinedString.GetHashCode();
+
+            // Convert the hash code to a positive number
+            int positiveHashCode = Math.Abs(hashCode);
+
+            // Take the last 6 digits of the positive hash code
+            string otp = positiveHashCode.ToString().Substring(positiveHashCode.ToString().Length - 6);
+
+            Console.WriteLine("Validity Period: " + dateTime.AddSeconds(ValidityPeriodSeconds).ToString("yyyy-MM-dd HH:mm:ss"));
+
+            return otp;
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            // Example usage
+            string userId = "exampleUser";
+            DateTime dateTime = DateTime.Now;
+
+            OneTimePasswordGenerator otpGenerator = new OneTimePasswordGenerator();
+            string otp = otpGenerator.GenerateOTP(userId, dateTime);
+
+            Console.WriteLine("Generated OTP: " + otp);
+        }
+    }
+}
